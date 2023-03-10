70c78a996623475b3e462724701d762e5cccfbf7
Bryan Salva <salvatorresb@gmail.com>
2023-03-09 19:12:33 -0500

    Se Termino de agregar la conversion de la temperatura

--- Conversor/src/main/java/vista/MenuTemperaturas.java
+++ Conversor/src/main/java/vista/MenuTemperaturas.java
@@ -7,44 +7,13 @@ import javax.swing.JOptionPane;
  *
  * @author Lenovo
  */
-public class Temperatura extends javax.swing.JFrame {
-    
-    public static double temperatura;
+public class MenuTemperaturas extends javax.swing.JFrame {
 
-    public static double getTemperatura() {
-        return temperatura;
-    }
-
-    public static void setTemperatura(double temperatura) {
-        Temperatura.temperatura = temperatura;
-    }
-    
-    public static String celiuskelvin(){
-        return ": La temperatura es "+String.format("%.3f",273.15+Temperatura.temperatura)+"grados kelvin";
-    }
-    public static String celciusfarenhgeith(){
-        return ": La temperatura es "+String.format("%.3f",32+1.8*Temperatura.temperatura)+"grados farenhgeith";
-    }
-    public static String kelvincelcius(){
-        return ": La temperatura es "+String.format("%.3f",Temperatura.temperatura-273.15)+"grados celcius";
-    }
-    public static String kelvinfarenhgeith(){
-        return ": La temperatura es "+String.format("%.3f",1.8*(Temperatura.temperatura-273.15)+32)+"grados farenhgeith";
-    }
-    public static String farenhgeithcelcius(){
-        return ": La temperatura es "+String.format("%.3f",(Temperatura.temperatura-32)*1.8)+"grados celcius";
-    }
-    public static String farenhgeithkelvin(){
-        return ": La temperatura es "+String.format("%.3f",(Temperatura.temperatura+459.67)*1.8)+"grados kelvin";
-    }
     /**
-     * Creates new form Temperatura
+     * Creates new form MenuTemperaturas
      */
-    public Temperatura() {
+    public MenuTemperaturas() {
         initComponents();
-        this.setTitle("Input Temperatura");
-        this.setLocationRelativeTo(Temperatura.this);
-        this.setResizable(false);
     }
 
     /**
@@ -58,7 +27,7 @@ public class Temperatura extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        txt_inputemperatura = new javax.swing.JTextField();
+        cmb_temperatura = new javax.swing.JComboBox<>();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
 
@@ -67,10 +36,12 @@ public class Temperatura extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(0, 102, 102));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(204, 255, 255));
-        jLabel1.setText("Ingrese la temperatura a convertir");
+        jLabel1.setForeground(new java.awt.Color(153, 255, 255));
+        jLabel1.setText("SELECCIONE LA CONVERCIÓN");
 
-        jButton1.setBackground(new java.awt.Color(255, 153, 0));
+        cmb_temperatura.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "celius a kelvin", "celcius a farenhgeith", "kelvin a celcius", "kelvin a farenhgeith", "farenhgeith a celcius", "farenhgeith a kelvin" }));
+
+        jButton1.setBackground(new java.awt.Color(255, 102, 0));
         jButton1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jButton1.setText("OK");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -79,8 +50,8 @@ public class Temperatura extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setBackground(new java.awt.Color(255, 51, 0));
         jButton2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 51, 0));
         jButton2.setText("CANCELAR");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -95,30 +66,30 @@ public class Temperatura extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(109, 109, 109)
+                        .addGap(75, 75, 75)
                         .addComponent(jLabel1))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(41, 41, 41)
+                        .addGap(32, 32, 32)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jButton1)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jButton2))
-                            .addComponent(txt_inputemperatura, javax.swing.GroupLayout.PREFERRED_SIZE, 320, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(39, Short.MAX_VALUE))
+                            .addComponent(cmb_temperatura, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(28, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(28, 28, 28)
+                .addGap(41, 41, 41)
                 .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(txt_inputemperatura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
+                .addComponent(cmb_temperatura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2))
-                .addContainerGap(22, Short.MAX_VALUE))
+                .addContainerGap(31, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -135,31 +106,53 @@ public class Temperatura extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        Menu menu =new Menu();
-        menu.setVisible(true);
-        dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
-
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        String temp = txt_inputemperatura.getText();
+        Finalizar fin = new Finalizar();
+        String cmbtemp = (String)cmb_temperatura.getSelectedItem();
         
-        if(temp.length()==0 ){
-            JOptionPane.showMessageDialog(this,"Ingrese la temperatura");
-        } else {
-            Double temprtura = Double.parseDouble(temp);
-            if(temprtura>0){
-                setTemperatura(temprtura);
-                MenuTemperaturas mm =new MenuTemperaturas();
-                mm.setVisible(true);
-                dispose();
-            }else{
-                JOptionPane.showMessageDialog(this,"Ingrese una cantidad Positiva");
-            }
-        }  
+        switch (cmbtemp) {
+            case "celius a kelvin" :
+                JOptionPane.showMessageDialog(this,"Celcius a Kelvin: "+Temperatura.celiuskelvin());
+                fin.setVisible(true);
+                this.dispose();
+                break;
+            case "celcius a farenhgeith" :
+                JOptionPane.showMessageDialog(this,"celcius a farenhgeith"+Temperatura.celciusfarenhgeith());
+                fin.setVisible(true);
+                this.dispose();
+                break;
+            case "kelvin a celcius" :
+                JOptionPane.showMessageDialog(this,"kelvin a celcius"+Temperatura.kelvincelcius());
+                fin.setVisible(true);
+                this.dispose();
+                break;
+            case "kelvin a farenhgeith" :
+                JOptionPane.showMessageDialog(this,"kelvin a farenhgeith"+Temperatura.kelvinfarenhgeith());
+                fin.setVisible(true);
+                this.dispose();
+                break;
+            case "farenhgeith a celcius" :
+                JOptionPane.showMessageDialog(this,"farenhgeith a celcius"+Temperatura.farenhgeithcelcius());
+                fin.setVisible(true);
+                this.dispose();
+                break;
+            case "farenhgeith a kelvin" :
+                JOptionPane.showMessageDialog(this,"farenhgeith a kelvin"+Temperatura.farenhgeithkelvin());
+                fin.setVisible(true);
+                this.dispose();
+                break;
+            default :
+                throw new AssertionError();
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        Temperatura tem = new Temperatura();
+        tem.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -178,29 +171,29 @@ public class Temperatura extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Temperatura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuTemperaturas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Temperatura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuTemperaturas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Temperatura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuTemperaturas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Temperatura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuTemperaturas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Temperatura().setVisible(true);
+                new MenuTemperaturas().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> cmb_temperatura;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField txt_inputemperatura;
     // End of variables declaration//GEN-END:variables
 }
